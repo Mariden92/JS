d506ddafa848074456fd191fb8ff1355739b4415
Mariden92 <92917668+Mariden92@users.noreply.github.com>
2022-01-05 18:27:33 +0300

    Add files via upload

--- Задание 2.js
+++ Задание 2.js
@@ -1,10 +1,56 @@
-let x;
-switch (X) {
-    case typeof x==="number":
-        console.log('результат равен number');
-        break;
-    case typeof x==="string":
-        console.log('результат равен 1');
-        break;
+let x = 4;
+if (typeof x==='number') {
+    console.log('Х-число')
+}
+else if(typeof x==='string'){
+    console.log('Х-строка')
+}
+else if(typeof x==='boolean'){
+    console.log('Х-логический тип')
+}
+else {
+    console.log('Тип не определен')
+}
 
+let x = true;
+if (typeof x==='number') {
+    console.log('Х-число')
+}
+else if(typeof x==='string'){
+    console.log('Х-строка')
+}
+else if(typeof x==='boolean'){
+    console.log('Х-логический тип')
+}
+else {
+    console.log('Тип не определен')
+}
+
+let x = "";
+if (typeof x==='number') {
+    console.log('Х-число')
+}
+else if(typeof x==='string'){
+    console.log('Х-строка')
+}
+else if(typeof x==='boolean'){
+    console.log('Х-логический тип')
+}
+else {
+    console.log('Тип не определен')
+}
+
+
+let x;
+if (typeof x==='number') {
+    console.log('Х-число')
+}
+else if(typeof x==='string'){
+    console.log('Х-строка')
+}
+else if(typeof x==='boolean'){
+    console.log('Х-логический тип')
+}
+else {
+    console.log('Тип не определен')
 }
