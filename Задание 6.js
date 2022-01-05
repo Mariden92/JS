b2f5fdfb7d4dd4558ecc6cbf0673fe6cd4c55be5
Mariden92 <92917668+Mariden92@users.noreply.github.com>
2022-01-05 18:38:14 +0300

    Add files via upload

--- Задание 6.js
+++ Задание 6.js
@@ -8,3 +8,15 @@ for (let i = 1; i < arr.length; i++) {
     console.log('false')
   }
 }
+
+
+let arr = [1, 1, 1, 1, 1]
+
+for (let i = 1; i < arr.length; i++) {
+    let value = arr[0]
+  if (value == arr[i]) {
+    console.log('true')
+  } else {
+    console.log('false')
+  }
+}
